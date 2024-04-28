f8d079e0b5c602f67b64264bf3b52dbd92020feb
Matthew <mcl244@cornell.edu>
2024-04-28 19:05:09 -0400

    Implement piece_valid_moves
    Implements piece_valid_moves in Movement which returns all valid squares a piece can go to.

--- chess_game/lib/movement.ml
+++ chess_game/lib/movement.ml
@@ -209,3 +209,19 @@ let castle start_row start_col end_row end_col =
       has_moved.(5) <- true;
       "bqsc" (* Black queenside castle*)
   | _ -> ""
+
+let board_list =
+  [
+    (0, 1); (0, 2); (0, 3); (0, 4); (0, 5); (0, 6); (0, 7); (1, 1); (1, 2);
+    (1, 3); (1, 4); (1, 5); (1, 6); (1, 7); (2, 1); (2, 2); (2, 3); (2, 4);
+    (2, 5); (2, 6); (2, 7); (3, 1); (3, 2); (3, 3); (3, 4); (3, 5); (3, 6);
+    (3, 7); (4, 1); (4, 2); (4, 3); (4, 4); (4, 5); (4, 6); (4, 7); (5, 1);
+    (5, 2); (5, 3); (5, 4); (5, 5); (5, 6); (5, 7); (6, 1); (6, 2); (6, 3);
+    (6, 4); (6, 5); (6, 6); (6, 7); (7, 1); (7, 2); (7, 3); (7, 4); (7, 5);
+    (7, 6); (7, 7);
+  ]
+
+let piece_valid_moves piece start_row start_col state =
+  List.filter
+    (fun e -> is_valid_move piece start_row start_col (fst e) (snd e) state)
+    board_list
--- chess_game/lib/movement.mli
+++ chess_game/lib/movement.mli
@@ -4,10 +4,16 @@
 val is_valid_move :
   string -> int -> int -> int -> int -> string option array array -> bool
 
-(** [can_castle start_row start_col end_row end_col state] is true if
-    player inputs a valid castle. *)
+(** [can_castle start_row start_col end_row end_col state] is true if player
+    inputs a valid castle. *)
 val can_castle : int -> int -> int -> int -> string option array array -> bool
-(** [castle start_row start_col end_row end_col] is the string representation
-    of what type of castle was made. For example "wksc" stands for a white
-    king side castle. *)
+
+(** [castle start_row start_col end_row end_col] is the string representation of
+    what type of castle was made. For example "wksc" stands for a white king
+    side castle. *)
 val castle : int -> int -> int -> int -> string
+
+(** [piece_valid moves piece start_row start_col state] is a list of all of the
+    legal moves a piece can move to on a board. *)
+val piece_valid_moves :
+  string -> int -> int -> string option array array -> (int * int) list
