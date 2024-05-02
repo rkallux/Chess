4b1390ca3abd365cb4bd7115188142996f998f3a
Alexander He <adh236@cornell.edu>
2024-05-02 00:37:55 -0400

    Cleaned up main
    -changed some names
    -new functions that factor out code
    -didnt touch promotion

--- chess_game/bin/main.ml
+++ chess_game/bin/main.ml
@@ -8,30 +8,6 @@ let _ = GtkMain.Main.init ()
 let width = 600
 
 let height = 600
-
-let state =
-  [|
-    [|
-      Some "B_Rook"; Some "B_Knight"; Some "B_Bishop"; Some "B_Queen";
-      Some "B_King"; Some "B_Bishop"; Some "B_Knight"; Some "B_Rook";
-    |];
-    [|
-      Some "B_Pawn"; Some "B_Pawn"; Some "B_Pawn"; Some "B_Pawn"; Some "B_Pawn";
-      Some "B_Pawn"; Some "B_Pawn"; Some "B_Pawn";
-    |]; [| None; None; None; None; None; None; None; None |];
-    [| None; None; None; None; None; None; None; None |];
-    [| None; None; None; None; None; None; None; None |];
-    [| None; None; None; None; None; None; None; None |];
-    [|
-      Some "W_Pawn"; Some "W_Pawn"; Some "W_Pawn"; Some "W_Pawn"; Some "W_Pawn";
-      Some "W_Pawn"; Some "W_Pawn"; Some "W_Pawn";
-    |];
-    [|
-      Some "W_Rook"; Some "W_Knight"; Some "W_Bishop"; Some "W_Queen";
-      Some "W_King"; Some "W_Bishop"; Some "W_Knight"; Some "W_Rook";
-    |];
-  |]
-
 let buttons = Array.make_matrix 8 8 (GButton.button ~label:"" ())
 
 type f = {
@@ -41,51 +17,62 @@ type f = {
 
 let prev = { row = 4; col = 4 }
 
-(**[piece_square r c] is the type of piece at row [r] and column [c] at the
-   beginning of the game*)
-let piece_square row col =
-  match state.(row).(col) with
-  | Some piece -> piece
-  | None -> ""
-
-(**[set_square_img r c] generates the image to be shown at row [r] and column
-   [c] as specified by [piece_square]*)
-let set_square_img row col =
+(**[gen_pixbuf r c] generates the image to be shown at row [r] and column [c] as
+   specified by [Movement.piece_square_ind r c]*)
+let gen_pixbuf row col =
   let dim = 65 in
   let img = GdkPixbuf.create ~width:dim ~height:dim ~has_alpha:true () in
   GdkPixbuf.scale ~dest:img ~width:dim ~height:dim
-    (GdkPixbuf.from_file ("assets/" ^ piece_square row col ^ ".png"));
+    (GdkPixbuf.from_file
+       ("assets/" ^ Movement.piece_square_ind row col ^ ".png"));
   img
 
-let do_castle button c ke rs re =
-  (* color, king end, rook start, rook end*)
-  ignore
-    (GMisc.image
-       ~pixbuf:(set_square_img prev.row prev.col)
-       ~packing:button#set_image ());
-  ignore (GMisc.image ~packing:buttons.(prev.row).(prev.col)#set_image ());
-  ignore
-    (GMisc.image ~pixbuf:(set_square_img c rs)
-       ~packing:buttons.(c).(re)#set_image ());
-  ignore (GMisc.image ~packing:buttons.(c).(rs)#set_image ());
-  state.(c).(ke) <- state.(c).(4);
-  state.(c).(4) <- None;
-  state.(c).(re) <- state.(c).(rs);
-  state.(c).(rs) <- None
+let set_img location pix =
+  ignore (GMisc.image ~pixbuf:pix ~packing:location#set_image ())
 
-let captured_W = Array.make 15 (GMisc.image ~width:10 ~height:100 ())
-let captured_B = Array.make 15 (GMisc.image ~width:10 ~height:100 ())
+let rm_img location = ignore (GMisc.image ~packing:location#set_image ())
 
-let rec update_tab (cap_pieces : GMisc.image array) lst n =
-  match lst with
-  | [] -> ()
-  | h :: t ->
-      let dim = 30 in
-      let img = GdkPixbuf.create ~width:dim ~height:dim ~has_alpha:true () in
-      GdkPixbuf.scale ~dest:img ~width:dim ~height:dim
-        (GdkPixbuf.from_file ("assets/" ^ h ^ ".png"));
-      cap_pieces.(n)#set_pixbuf img;
-      update_tab cap_pieces t (n + 1)
+(**[castle_gui color king_end rook_start rook_end] updates the gui to perform a
+   castle*)
+let castle_gui (button : GButton.button) prev_row prev_col row col =
+  let perform_castle (button : GButton.button) c ke rs re =
+    (* color, king end, rook start, rook end*)
+    set_img button (gen_pixbuf prev.row prev.col);
+    rm_img buttons.(prev.row).(prev.col);
+    set_img buttons.(c).(re) (gen_pixbuf c rs);
+    rm_img buttons.(c).(rs);
+    Movement.castle_state c ke rs re
+  in
+
+  match Movement.type_castle prev_row prev_col row col with
+  | "wksc" -> perform_castle button 7 6 7 5
+  | "wqsc" -> perform_castle button 7 2 0 3
+  | "bksc" -> perform_castle button 0 6 7 5
+  | "bqsc" -> perform_castle button 0 2 0 3
+  | _ -> failwith "Should never reach this case"
+
+(**[captured_W_img] stores the images of captured white pieces*)
+let captured_W_img = Array.make 15 (GMisc.image ~width:10 ~height:100 ())
+
+(**[captured_W_img] stores the images of captured black pieces*)
+let captured_B_img = Array.make 15 (GMisc.image ~width:10 ~height:100 ())
+
+(**[update_captures captured_images captures pos] updates the gui to reflect the
+   captured pieces in [captures]*)
+let update_captures_gui () =
+  let rec update_cap_gui (captured_img : GMisc.image array) lst n =
+    match lst with
+    | [] -> ()
+    | h :: t ->
+        let dim = 30 in
+        let img = GdkPixbuf.create ~width:dim ~height:dim ~has_alpha:true () in
+        GdkPixbuf.scale ~dest:img ~width:dim ~height:dim
+          (GdkPixbuf.from_file ("assets/" ^ h ^ ".png"));
+        captured_img.(n)#set_pixbuf img;
+        update_cap_gui captured_img t (n + 1)
+  in
+  update_cap_gui captured_B_img !Movement.captured_B 0;
+  update_cap_gui captured_W_img !Movement.captured_W 0
 
 (** [promote_pawn] creates a dialog box that allows the user to choose which
     piece to promote a pawn to. *)
@@ -111,17 +98,63 @@ let promote_pawn color =
   print_endline "Done";
   !result
 
+(* Function to create a square *)
+let create_square row col =
+  let button = GButton.button ~label:"" () in
+  if Movement.has_piece row col then set_img button (gen_pixbuf row col);
+  let color = if (row + col) mod 2 = 0 then `NAME "white" else `NAME "green" in
+  button#misc#modify_bg [ (`NORMAL, color) ];
+  let _ =
+    button#connect#clicked ~callback:(fun () ->
+        (* if the move from the previous square to the clicked square is
+           valid *)
+        if Movement.valid_move prev.row prev.col row col then
+          let piece = Movement.piece_square_ind prev.row prev.col in
+
+          (**********PROMOTION*********)
+          if (piece = "W_Pawn" && row = 0) || (piece = "B_Pawn" && row = 7) then (
+            let new_piece = promote_pawn (String.sub piece 0 1) in
+            Movement.promote prev.row prev.col new_piece;
+            Movement.update_captures row col;
+            update_captures_gui ();
+            set_img button (gen_pixbuf prev.row prev.col);
+            rm_img buttons.(prev.row).(prev.col);
+            Movement.update_state prev.row prev.col row col
+            (**********PROMOTION********))
+          else if Movement.is_valid_castle prev.row prev.col row col then
+            castle_gui button prev.row prev.col row col
+          else (
+            (* regular move or capture *)
+
+            (* update captures table, if necessary*)
+            Movement.update_captures row col;
+            update_captures_gui ();
+
+            (* Then set new piece at new square and remove old image*)
+            set_img button (gen_pixbuf prev.row prev.col);
+            rm_img buttons.(prev.row).(prev.col);
+
+            (* update state *)
+            Movement.update_state prev.row prev.col row col)
+        else (
+          (* didn't click on a piece *)
+          prev.row <- row;
+          prev.col <- col))
+  in
+  button
+
 (**[create_chessboard_window] creates a window with a standard chess board setup*)
 let create_chessboard_window () =
+  (* Main window *)
   let window = GWindow.window ~width ~height ~title:"Board" () in
-  ignore (window#connect#destroy ~callback:Main.quit);
+  let _ = window#connect#destroy ~callback:Main.quit in
 
   (* Vertical box for the Quit button + chessboard *)
   let vbox = GPack.vbox ~width ~height ~packing:window#add () in
 
   (* Quit button *)
   let quit_button = GButton.button ~label:"Quit" ~packing:vbox#pack () in
-  ignore (quit_button#connect#clicked ~callback:Main.quit);
+  let _ = quit_button#connect#clicked ~callback:Main.quit in
 
   (* Table for chessbaord *)
   let tableB =
@@ -130,7 +163,7 @@ let create_chessboard_window () =
   for cap_p = 0 to 14 do
     let image = GMisc.image ~width:10 ~height:100 () in
     tableB#attach ~left:cap_p ~top:0 ~expand:`BOTH ~fill:`BOTH image#coerce;
-    captured_B.(cap_p) <- image
+    captured_B_img.(cap_p) <- image
   done;
 
   let table =
@@ -143,78 +176,10 @@ let create_chessboard_window () =
   for cap_p = 0 to 14 do
     let image = GMisc.image ~width:10 ~height:100 () in
     tableW#attach ~left:cap_p ~top:0 ~expand:`BOTH ~fill:`BOTH image#coerce;
-    captured_W.(cap_p) <- image
+    captured_W_img.(cap_p) <- image
   done;
 
-  (* Function to create a square *)
-  let create_square row col =
-    let button = GButton.button ~label:"" () in
-    if piece_square row col != "" then
-      ignore
-        (GMisc.image ~pixbuf:(set_square_img row col) ~packing:button#set_image
-           ());
-    let color =
-      if (row + col) mod 2 = 0 then `NAME "white" else `NAME "green"
-    in
-    button#misc#modify_bg [ (`NORMAL, color) ];
-    ignore
-      (button#connect#clicked ~callback:(fun () ->
-           let piece = piece_square prev.row prev.col in
-           (* If prev square is a piece *)
-           if
-             piece_square prev.row prev.col <> ""
-             && Movement.play_move piece prev.row prev.col row col state
-           then
-             if (piece = "W_Pawn" && row = 0) || (piece = "B_Pawn" && row = 7)
-             then (
-               let new_piece = promote_pawn (String.sub piece 0 1) in
-               state.(prev.row).(prev.col) <- Some new_piece;
-               Movement.update_captures row col state;
-               update_tab captured_B !Movement.captured_B 0;
-               update_tab captured_W !Movement.captured_W 0;
-               ignore
-                 (GMisc.image
-                    ~pixbuf:(set_square_img prev.row prev.col)
-                    ~packing:button#set_image ());
-               ignore
-                 (GMisc.image ~packing:buttons.(prev.row).(prev.col)#set_image
-                    ());
-               state.(row).(col) <- state.(prev.row).(prev.col);
-               state.(prev.row).(prev.col) <- None)
-             else (
-               (*Update captures, if necessary, and update the table*)
-               Movement.update_captures row col state;
-               update_tab captured_B !Movement.captured_B 0;
-               update_tab captured_W !Movement.captured_W 0;
-               (* Then set new piece at new square *)
-               ignore
-                 (GMisc.image
-                    ~pixbuf:(set_square_img prev.row prev.col)
-                    ~packing:button#set_image ());
-               (* Change prev piece to blank *)
-               ignore
-                 (GMisc.image ~packing:buttons.(prev.row).(prev.col)#set_image
-                    ());
-               (* move prev piece to new location *)
-               state.(row).(col) <- state.(prev.row).(prev.col);
-               state.(prev.row).(prev.col) <- None)
-           else if
-             (piece_square prev.row prev.col = "B_King"
-             || piece_square prev.row prev.col = "W_King")
-             && Movement.can_castle prev.row prev.col row col state
-           then
-             match Movement.castle prev.row prev.col row col with
-             | "wksc" -> do_castle button 7 6 7 5
-             | "wqsc" -> do_castle button 7 2 0 3
-             | "bksc" -> do_castle button 0 6 7 5
-             | "bqsc" -> do_castle button 0 2 0 3
-             | _ -> failwith "Should never reach this case"
-           else (
-             prev.row <- row;
-             prev.col <- col)));
-
-    button
-  in
+  (* attach buttons to each square in table *)
   for row = 0 to 7 do
     for col = 0 to 7 do
       let square = create_square row col in
@@ -229,7 +194,7 @@ let create_chessboard_window () =
 (**[create_homescreen_window] creates the inital wondow for game mode selection*)
 let create_homescreen_window () =
   let window = GWindow.window ~width ~height ~title:"Home Screen" () in
-  ignore (window#connect#destroy ~callback:Main.quit);
+  let _ = window#connect#destroy ~callback:Main.quit in
 
   let vbox = GPack.vbox ~packing:window#add () in
 
@@ -237,15 +202,17 @@ let create_homescreen_window () =
   two_player_button#misc#set_size_request ~width:100 ~height:100 ();
   two_player_button#misc#modify_bg [ (`NORMAL, `NAME "gray") ];
   two_player_button#set_border_width 10;
-  ignore
-    (let lbl = GMisc.label ~packing:two_player_button#set_image () in
-     lbl#set_text "Two Player";
-     lbl#set_justify `CENTER);
-  ignore
-    (two_player_button#connect#clicked ~callback:(fun () ->
-         window#destroy ();
-         create_chessboard_window ();
-         Main.main ()));
+  let _ =
+    let lbl = GMisc.label ~packing:two_player_button#set_image () in
+    lbl#set_text "Two Player";
+    lbl#set_justify `CENTER
+  in
+  let _ =
+    two_player_button#connect#clicked ~callback:(fun () ->
+        window#destroy ();
+        create_chessboard_window ();
+        Main.main ())
+  in
 
   window#show ();
   ()
